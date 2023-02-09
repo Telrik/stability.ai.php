66c5ff6365a333daa2e63b696e59371910d884ed
Telrik <fs@m7.ru>
2023-02-09 09:36:24 +0000

    Minor update

--- git_push.sh
+++ git_push.sh
@@ -16,12 +16,12 @@ if [ "$git_host" = "" ]; then
 fi
 
 if [ "$git_user_id" = "" ]; then
-    git_user_id="GIT_USER_ID"
+    git_user_id="telrik"
     echo "[INFO] No command line input provided. Set \$git_user_id to $git_user_id"
 fi
 
 if [ "$git_repo_id" = "" ]; then
-    git_repo_id="GIT_REPO_ID"
+    git_repo_id="stability.ai.php"
     echo "[INFO] No command line input provided. Set \$git_repo_id to $git_repo_id"
 fi
 
